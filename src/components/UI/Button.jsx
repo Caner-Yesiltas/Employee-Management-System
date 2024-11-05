fbbc99515392125aa2338bbd5b86b5779a7ebf13
CanerYesiltas <caneryesiltas1@gmail.com>
2024-11-05 18:33:37 -0500

    add button component with props.type

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Button = (props) => {
   return (
     <div>
-      <button
+      <button       type={props.type || "button"}
         className={`p-2 text-lg bg-black text-white mt-2 w-full max-w-[40rem] mx-auto  ${props.className} `}
       >
-        
+  
         {props.children}
       </button>
     </div>
--- src/components/Workers/AddWorker.jsx
+++ src/components/Workers/AddWorker.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 
 const AddWorker = () => {
+  const [enteredWorkerName, setEnteredWorkerName] = useState('');
+
+  const [enteredWAGE, setEnteredWage] = useState('');
+
+  const workerNameChangeHandler = (e) => {
+    setEnteredWorkerName(e.target.value);
+  };
+
+  const wageChangeHandler = (e) => {
+    setEnteredWage(e.target.value);
+  };
+
+  const addWorkerHandler = (e) => {
+    e.preventDefault();
+
+    console.log(enteredWorkerName, enteredWAGE);
+  };
+
   return (
     <Card addClass='mt-10 bg-blue-500'>
-      <form className='flex flex-col gap-y-2'>
+      <form className='flex flex-col gap-y-2' onSubmit={addWorkerHandler}>
         <label htmlFor='name' className='font-medium'>
           Worker Name
         </label>
@@ -14,6 +32,7 @@ const AddWorker = () => {
           className='max-w-[40rem] w-full mx-auto border p-2'
           placeholder='Enter employee name'
           id='name'
+          onChange={workerNameChangeHandler}
         />
 
         <label htmlFor='wage'>Salary</label>
@@ -22,11 +41,10 @@ const AddWorker = () => {
           className='max-w-[40rem] w-full mx-auto border p-2'
           placeholder='Enter your salary'
           id='wage'
+          onChange={wageChangeHandler}
         />
 
-        <Button className='mt-3' >
-            
-            Add</Button>
+        <Button className='mt-3' type="submit"   >Add</Button>
       </form>
     </Card>
   );
